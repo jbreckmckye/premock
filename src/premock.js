55941f3fe2a52c9a657b28a82111c447e631842e
jbreckmckye <jimmy@breck-mckye.com>
2015-10-16 22:47:07 +0100

    Allow consumer to create persisted premockers

--- src/premock.js
+++ src/premock.js
@@ -2,22 +2,30 @@ module.exports = premock;
 
 var MaybeFunction = require('./MaybeFunction.js');
 var HeapCallStore = require('./HeapCallStore.js');
+var LocalCallStore = require('./LocalCallStore.js');
 var createProxy = require('./createProxy.js');
 var replayCalls = require('./replayCalls.js');
 
 function premock(promise) {
+    return createPremocker(new HeapCallStore(), promise);
+}
+
+premock.withPersistence = function premockWithPersistence(name, promise) {
+    return createPremocker(new LocalCallStore(name), promise);
+};
+
+function createPremocker(callStore, implementationPromise) {
 	var maybeFunction = new MaybeFunction(onImplemented);
-	var heapCallStore = new HeapCallStore();
-	var proxy = createProxy(maybeFunction.getImplementation, heapCallStore);
+	var proxy = createProxy(maybeFunction.getImplementation, callStore);
 
 	proxy.resolve = maybeFunction.resolveImplementation;
-	if (promise && promise.then) {
-		promise.then(maybeFunction.resolveImplementation);
+	if (implementationPromise && implementationPromise.then) {
+		implementationPromise.then(maybeFunction.resolveImplementation);
 	}
 
 	return proxy;
 
 	function onImplemented(implementation) {
-		replayCalls(heapCallStore.getCalls(), implementation);
+		replayCalls(callStore.getCalls(), implementation);
 	}
 }
