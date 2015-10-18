757fac29674faa1f0118ba9aebabb376dd87e1de
jbreckmckye <jimmy@breck-mckye.com>
2015-10-18 20:22:13 +0100

    Add two refactor notes. Fixes to these should be forthcoming.

--- src/replayCalls.js
+++ src/replayCalls.js
@@ -11,6 +11,8 @@ function replayCalls(calls, implementation) {
 		var result;
 		var thisBinding = call.thisBinding || null; // Calls from previous pages will lack bindings
 		defer(function(){
+            // todo - there is a bug here, where a faulty set of params in localStorage will never clear, because onExecuted isn't called if the implementation throws an error
+
 			result = implementation.apply(thisBinding, call.callArguments);
 			if (call.onExecuted) {
                 call.onExecuted(result);
