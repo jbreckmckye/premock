e721475d81fe2c43b4231ea023019d0ad8801298
Jimmy Breck-McKye <jimmy@breck-mckye.com>
2015-11-11 23:26:27 +0000

    Docs

--- src/createFunctionProxy.js
+++ src/createFunctionProxy.js
@@ -3,6 +3,9 @@ module.exports = createFunctionProxy;
 // Ghetto dependency injection
 createFunctionProxy._Promise = window.Promise || null;
 
+// Create a proxy for our future function.
+// The proxy will route calls to either the real implementation - if it exists -
+// or the call storage object.
 function createFunctionProxy(getImplementation, callStore) {
 	return function functionProxy() {
 		var Promise = createFunctionProxy._Promise;
