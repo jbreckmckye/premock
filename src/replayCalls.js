b879cf0698bc9836d80dffb4380073bfd20ec9b7
Jimmy Breck-McKye <jimmy@breck-mckye.com>
2015-11-11 23:32:11 +0000

    Exceptions should not stop call data being deleted
    Previously, if a call replay threw exceptions, that would stop the replay data being cleared. This means that premock would get 'stuck' on the faulty call.

--- src/replayCalls.js
+++ src/replayCalls.js
@@ -11,12 +11,20 @@ function replayCalls(calls, implementation) {
 		var result;
 		var thisBinding = call.thisBinding || null; // Calls from previous pages will lack bindings
 		defer(function(){
-            // todo - there is a bug here, where a faulty set of params in localStorage will never clear, because onExecuted isn't called if the implementation throws an error
-
-			result = implementation.apply(thisBinding, call.callArguments);
+			var errors;
+			try {
+			    result = implementation.apply(thisBinding, call.callArguments);
+			} catch (e) {
+			    errors = e;
+			}
+			
 			if (call.onExecuted) {
-                call.onExecuted(result);
-            }
+                            call.onExecuted(result);
+                        }
+                        
+                        if (errors) {
+                            throw errors;
+                        }
 		});
 	});
 }
