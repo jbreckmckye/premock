2b7e80367c05dd8013550133422472be6fce75b7
jbreckmckye <jimmy@breck-mckye.com>
2016-02-13 17:24:06 +0000

    Rename files

---
+++ src/api.js
@@ -0,0 +1,43 @@
+// Public exports
+module.exports =    premock;
+                    premock.withoutPersistence = premockWithoutPersistence;
+
+var MaybeFunction = require('./MaybeFunction.js');
+var HeapCallStore = require('./HeapCallStore.js');
+var LocalCallStore = require('./LocalCallStore.js');
+var createFunctionProxy = require('./createFunctionProxy.js');
+var replayCalls = require('./replayCalls.js');
+var canUseLocalStorage = require('./canUseLocalStorage.js');
+
+function premock(name, promise) {
+    if (name === undefined) {
+        throw new Error('Premock: needs a storage ID key');
+    }
+    
+    if (canUseLocalStorage() === false) {
+        throw new Error('Premock: did not detect localStorage');
+    }
+
+    var callStore = new LocalCallStore(name.toString());
+    return createPremocker(callStore, promise);
+}
+
+function premockWithoutPersistence(promise) {
+	return createPremocker(new HeapCallStore(), promise);
+}
+
+function createPremocker(callStore, implementationPromise) {
+	var maybeFunction = new MaybeFunction();
+	var proxy = createFunctionProxy(maybeFunction.getImplementation, callStore);
+
+	proxy.resolve = function resolvePremock(implementation) {
+		maybeFunction.resolveImplementation(implementation);
+		replayCalls(callStore.getCalls(), implementation);
+	};
+
+	if (implementationPromise && implementationPromise.then) {
+		implementationPromise.then(proxy.resolve);
+	}
+
+	return proxy;
+}
